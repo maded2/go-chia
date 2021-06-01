c588626012c193cd62b8605100b0510779639baf
eddie <eddie@bitdeltaone.com>
2021-06-01 17:48:31 +0800

    test wallet api

--- pkg/gochia/ChiaConfig_test.go
+++ pkg/gochia/ChiaConfig_test.go
@@ -34,3 +34,67 @@ func TestCreateClient(t *testing.T) {
 	sb := string(body)
 	t.Logf(sb)
 }
+
+/*
+login to wallet
+*/
+func TestCreateClient2(t *testing.T) {
+	config := &ChiaConfig{
+		ChiaCertFile: "/home/eddie/.chia/mainnet/config/ssl/full_node/private_full_node.crt",
+		ChiaKeyFile:  "/home/eddie/.chia/mainnet/config/ssl/full_node/private_full_node.key",
+	}
+	client, err := config.CreateClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	postBody, _ := json.Marshal(map[string]interface{}{
+		"fingerprint": 123,
+		"type":        "start",
+		"host":        "https://localhost:9256",
+	})
+	responseBody := bytes.NewBuffer(postBody)
+	resp, err := client.Post("https://localhost:9256/log_in", "application/json", responseBody)
+	if err != nil {
+		t.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+	//Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sb := string(body)
+	t.Logf(sb)
+}
+
+/*
+get wallet balance
+*/
+func TestCreateClient3(t *testing.T) {
+	config := &ChiaConfig{
+		ChiaCertFile: "/home/eddie/.chia/mainnet/config/ssl/full_node/private_full_node.crt",
+		ChiaKeyFile:  "/home/eddie/.chia/mainnet/config/ssl/full_node/private_full_node.key",
+	}
+	client, err := config.CreateClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	postBody, _ := json.Marshal(map[string]interface{}{
+		"wallet_id": "1",
+		"type":      "start",
+		"host":      "https://localhost:9256",
+	})
+	responseBody := bytes.NewBuffer(postBody)
+	resp, err := client.Post("https://localhost:9256/get_wallet_balance", "application/json", responseBody)
+	if err != nil {
+		t.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+	//Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sb := string(body)
+	t.Logf(sb)
+}
